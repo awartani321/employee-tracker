720a0fa74489feba4927dc51d12e3587764f3ecc
Adam Awartani <adamawartani321@gmail.com>
2022-08-10 22:07:38 -0400

    add department, role, employee

--- index.js
+++ index.js
@@ -59,6 +59,16 @@ async function promptActions() {
 
         if (res.action == "View All Employees")
             await displayAllEmployees();
+
+        if (res.action == "Add A Department")
+            await addDepartment();
+
+        if (res.action == "Add A Role")
+            await addRole();
+
+        if (res.action == "Add An Employee")
+            await addEmployee();
+
     }
 }
 
@@ -87,6 +97,177 @@ async function displayAllEmployees() {
     console.log("")
     console.table(rows);
 }
+
+async function addDepartment() {
+    const res = await inquirer
+        .prompt([
+            //name
+            {
+                type: "input",
+                name: "name",
+                message: "What is the name of the department?",
+                validate: (val) => {
+                    if (val) {
+                        return true;
+                    } else {
+                        console.log("\nDepartment's name again.");
+                        return false;
+                    }
+                },
+            }
+        ]);
+
+    try {
+        await connection.execute(`INSERT INTO department (name) VALUES (?)`, [res.name]);
+        console.log(`Added ${res.name} to the database`);
+    } catch (err) {
+        console.log(`Failed to add department "${res.name}"`);
+    }
+
+}
+
+
+async function addRole() {
+    const res = await inquirer
+        .prompt([
+            //title
+            {
+                type: "input",
+                name: "title",
+                message: "What is the title of the role?",
+                validate: (val) => {
+                    if (val) {
+                        return true;
+                    } else {
+                        console.log("\nRole's title again.");
+                        return false;
+                    }
+                },
+            },
+            //salary
+            {
+                type: "input",
+                name: "salary",
+                message: "What is the salary of the role?",
+                validate: (val) => {
+                    if (val) {
+                        return true;
+                    } else {
+                        console.log("\nRole's salary again.");
+                        return false;
+                    }
+                },
+            },
+            //department
+            {
+                type: "input",
+                name: "department",
+                message: "Which department does the role belong to?",
+                validate: async (val) => {
+                    if (val) {
+                        const [rows, fields] = await connection.execute(`Select * From department Where name = ?`, [val]);
+
+                        if (rows.length < 1) {
+                            console.log("\nDepartment does not exist");
+                            return false;
+                        }
+                        return true;
+                    } else {
+                        console.log("\nRole's department again.");
+                        return false;
+                    }
+                },
+            }
+        ]);
+
+    try {
+        // find department id
+        const [rows, fields] = await connection.execute(`Select * From department Where name = ?`, [res.department]);
+        // console.log(rows)
+        await connection.execute(`INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`, [res.title, res.salary, rows[0]['id']]);
+        console.log(`Added ${res.title} to the database`);
+    } catch (err) {
+        console.log(`Failed to add department "${res.title}"`);
+    }
+
+}
+
+
+async function addEmployee() {
+    const res = await inquirer
+        .prompt([
+            //first_name
+            {
+                type: "input",
+                name: "first_name",
+                message: "What is the employee's first name?",
+                validate: (val) => {
+                    if (val) {
+                        return true;
+                    } else {
+                        console.log("\Employee's First Name again.");
+                        return false;
+                    }
+                },
+            },
+            //last_name
+            {
+                type: "input",
+                name: "last_name",
+                message: "What is the employee's last name?",
+                validate: (val) => {
+                    if (val) {
+                        return true;
+                    } else {
+                        console.log("\Employee's Last Name again.");
+                        return false;
+                    }
+                },
+            },
+            //role
+            {
+                type: "input",
+                name: "role",
+                message: "What is the employee's role?",
+                validate: async (val) => {
+                    if (val) {
+                        const [rows, fields] = await connection.execute(`Select * From role Where title = ?`, [val]);
+
+                        if (rows.length < 1) {
+                            console.log("\nRole does not exist");
+                            return false;
+                        }
+                        return true;
+                    } else {
+                        console.log("\nEmployee's Role again.");
+                        return false;
+                    }
+                },
+            },
+            //manager
+            {
+                type: "input",
+                name: "manager",
+                message: "What is the employee's manager?"
+            }
+        ]);
+
+    try {
+        // find department id
+        const [rows1, fields1] = await connection.execute(`Select * From role Where title = ?`, [res.role]);
+        const [rows2, fields2] = await connection.execute(`Select * From employee Where CONCAT(first_name, " ", last_name) = ?`, [res.manager]);
+        // console.log(rows1);
+        const manager_id = rows2.length > 0 ? rows2[0]['id'] : 0;
+        // console.log("manager_id", manager_id);
+        await connection.execute(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`, [res.first_name, res.last_name, rows1[0]['id'], manager_id]);
+        console.log(`Added ${res.first_name} ${res.last_name} to the database`);
+    } catch (err) {
+        console.log(`Failed to add department "${res.first_name} ${res.last_name}"`);
+    }
+
+}
+
+
 
 
 connectDatabase();
